bac883416fe59086be5655252a201f4c96d2cbd0
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-03 01:49:30 +1200

    fix: Use getResource() to resolve unit-test resource locations

--- src/test/java/TestDotParser.java
+++ src/test/java/TestDotParser.java
@@ -4,11 +4,16 @@ import project1.graph.DotParser;
 import project1.graph.Graph;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class TestDotParser {
     @Test
     public void testEmpty() throws IOException {
-        Graph g = DotParser.parse("target/test-classes/empty.dot");
+        // Pull a graph with no nodes from src/test/resources
+        URL empty = getClass().getResource("empty.dot");
+        Graph g = DotParser.parse(empty.getPath());
+
+        // Check that the graph name is correct and that there are no nodes or edges
         Assert.assertEquals(g.getName(), "empty");
         Assert.assertEquals(g.getEdges().size(), 0);
         Assert.assertEquals(g.getNodes().size(), 0);
