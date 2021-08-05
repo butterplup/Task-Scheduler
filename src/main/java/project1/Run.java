57422ef8f653b60aa5ec438144e6413c921a9fc9
Joel Hutchinson <joelkahuhutch@gmail.com>
2021-08-06 03:12:31 +1200

    feat: parse command line args, saving into an obj

--- src/main/java/project1/Run.java
+++ src/main/java/project1/Run.java
@@ -7,18 +7,24 @@ import java.io.IOException;
 /**
  * Entry point for the application
  */
-
 public class Run {
     public static void main(String[] args) throws IOException {
-        if (args.length == 1) {
-            String filename = args[0];
+        try {
+            ArgsParser argsParser = new ArgsParser(args);
+
+            String filename = argsParser.getFilename();
             Graph g = DotParser.parse(filename);
             g.print();
 
-            String inputName = filename.substring(0, filename.lastIndexOf("."));
-            DotParser.saveToFile(g, String.format("%s-output.dot", inputName));
-        } else {
-            System.out.printf("Usage: java -jar project1.jar DOT_FILE%n");
+            String outputFilename = argsParser.getOutputFilename();
+            DotParser.saveToFile(g, outputFilename);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO Make more explicit usage message?
+            System.out.printf("Usage: java -jar project1.jar DOT_FILE P [OPTIONAL args]%n ");
+            throw new IOException(e);
         }
+
     }
 }
