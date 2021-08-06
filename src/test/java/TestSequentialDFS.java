fff6eb52eba03af646f67aca59be6795eb5fe1bc
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-06 15:22:21 +1200

    feat: Introduce test cases to ensure that the DFS algorithm produces optimal schedules for the example task graphs.

--- src/test/java/TestSequentialDFS.java
+++ src/test/java/TestSequentialDFS.java
@@ -1,7 +1,77 @@
 import org.junit.Assert;
 import org.junit.Test;
+import project1.algorithm.Schedule;
 import project1.algorithm.SequentialDFS;
+import project1.graph.DotParser;
 import project1.graph.Graph;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class TestSequentialDFS {
+    private Schedule run(String resource, int processors) throws IOException {
+        URL url = getClass().getResource(resource);
+        Graph g = DotParser.parse(url.getPath());
+        return SequentialDFS.generateOptimalSchedule(g, processors);
+    }
+
+    @Test
+    public void test7Node2Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_7_OutTree.dot", 2);
+        Assert.assertEquals(28, s.getFinishTime());
+    }
+
+    @Test
+    public void test7Node4Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_7_OutTree.dot", 4);
+        Assert.assertEquals(22, s.getFinishTime());
+    }
+
+    @Test
+    public void test8Node2Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_8_Random.dot", 2);
+        Assert.assertEquals(581, s.getFinishTime());
+    }
+
+    @Test
+    public void test8Node4Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_8_Random.dot", 4);
+        Assert.assertEquals(581, s.getFinishTime());
+    }
+
+    @Test
+    public void test9Node2Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_9_SeriesParallel.dot", 2);
+        Assert.assertEquals(55, s.getFinishTime());
+    }
+
+    @Test
+    public void test9Node4Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_9_SeriesParallel.dot", 4);
+        Assert.assertEquals(55, s.getFinishTime());
+    }
+
+    @Test
+    public void test10Node2Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_10_Random.dot", 2);
+        Assert.assertEquals(50, s.getFinishTime());
+    }
+
+    @Test
+    public void test10Node4Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_10_Random.dot", 4);
+        Assert.assertEquals(50, s.getFinishTime());
+    }
+
+    @Test
+    public void test11Node2Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_11_OutTree.dot", 2);
+        Assert.assertEquals(350, s.getFinishTime());
+    }
+
+    @Test
+    public void test11Node4Processors() throws IOException {
+        Schedule s = run("exampleTaskGraphs/Nodes_11_OutTree.dot", 4);
+        Assert.assertEquals(227, s.getFinishTime());
+    }
 }
