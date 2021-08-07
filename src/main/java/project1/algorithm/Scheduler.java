ade09cefd9005daed43d06bb101a724d63fce51e
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-08 01:22:18 +1200

    refactor: Remove the Processor class, refactoring Schedule and Scheduler

--- src/main/java/project1/algorithm/Scheduler.java
+++ src/main/java/project1/algorithm/Scheduler.java
@@ -1,26 +1,24 @@
 package project1.algorithm;
 
 import project1.graph.Node;
-import project1.processor.Processor;
 
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * A scheduler for each sub-schedule is responsible for scheduling more
  * task nodes to the current sub-schedule.
  */
 public class Scheduler {
-    private List<Processor> processors;
     private Schedule current;
 
     /**
      * Constructor method for Scheduler
+     *
      */
     public Scheduler(Schedule c) {
-        this.processors=c.getProcessors();
         this.current=c;
     }
 
@@ -31,54 +29,47 @@ public class Scheduler {
      * @param scheduleStack All generated schedules will be added to the current schedule stack
      */
     public void scheduleTaskToProcessor(Node t,int best, Deque<Schedule> scheduleStack) { //current schedule+node t
-        int index = 0;
-        int emptyProcessorCount=0;
-        //Loop through all processors to find all possible schedules, every schedule is a potential solution
-        for (Processor p : this.processors) {
-            if (emptyProcessorCount>0){ //This is another empty processor-> WILL produce duplicate as previous schedule
-                continue;
+        boolean foundEmpty = false;
+
+        // Loop through all processors to find all possible schedules, every schedule is a potential solution
+        int index = -1;
+        for (int free : this.current.getFreeTime()) {
+            index++;
+
+            // Break if we encounter two empty processors
+            boolean empty = free == 0;
+            if (foundEmpty && empty){
+                break;
             }
-            if (p.isEmpty()){
-                emptyProcessorCount++;
-            }
+
+            foundEmpty = empty || foundEmpty;
+
             Schedule possibility=new Schedule(this.current); //deep copy
             int startTime;
             int communicationCost = 0;
             HashMap<Node, Integer> in = t.getIncomingEdges();
-            //find the earliest starting time on this processor p
+
+            // Find the earliest starting time on this processor p
             for (HashMap.Entry<Node, Integer> i : in.entrySet()) {
                 Node pre = i.getKey(); //get parent node
                 HashMap<String, TaskScheduled> scheduled = this.current.getCurrentSchedule(); //get current schedule
                 TaskScheduled predecessor = scheduled.get(pre.getName()); //lookup O(1)
-                if (predecessor.getProcessor() != p.getProcessorId()) { //communication cost
+                if (predecessor.getProcessor() != index) { //communication cost
                     communicationCost = Math.max(communicationCost, predecessor.getFinishTime() + i.getValue());
                 }
             }
-            startTime = Math.max(p.getEarliestStartTime(), communicationCost);
-            TaskScheduled scheduled = new TaskScheduled(t, startTime, this.processors.get(index).getProcessorId());
+            startTime = Math.max(free, communicationCost);
+            TaskScheduled scheduled = new TaskScheduled(t, startTime, index);
             possibility.addTask(scheduled);
             //Only add to Schedule to stack if its finish time<current best "complete" schedule
-            if (possibility.getFinishTime()<best) {
+            if (possibility.getFinishTime() < best) {
                 scheduleStack.push(possibility);
             }
-            index++;
         }
     }
 
-    /**
-     * This method finds the tasks which satisfies the scheduling constraints and is ready
-     * to be scheduled to a processor.
-     * @param taskList A list of tasks to be checked if they are ready to be scheduled.
-     * @return A list of tasks which satisfies the scheduling constraints and can be scheduled.
-     */
-    public List<Node> getTasksCanBeScheduled(List<Node> taskList){
-        List<Node> candidates=new LinkedList<>();
-        for (Node s:taskList) {
-            if (checkTaskCanBeScheduled(s)) {
-                candidates.add(s);
-            }
-        }
-        return candidates;
+    public Stream<Node> getTasksCanBeScheduled(List<Node> taskList){
+        return taskList.stream().filter(this::checkTaskCanBeScheduled);
     }
 
     /**
