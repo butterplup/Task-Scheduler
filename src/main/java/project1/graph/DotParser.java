b8c200e458899ce048d35bfb0afa39c2b715806f
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-08 18:22:20 +1200

    refactor: Alter GraphViz parser to use regex

--- src/main/java/project1/graph/DotParser.java
+++ src/main/java/project1/graph/DotParser.java
@@ -1,67 +1,122 @@
 package project1.graph;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.HashMap;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DotParser {
+    public enum LineType {
+        DIGRAPH,
+        NODE,
+        EDGE,
+        OTHER
+    }
+
+    // Regex patterns to match GraphViz content
+    private static final Pattern digraphPattern = Pattern.compile("digraph\\s*\"(.*)\" \\{");
+    private static final Pattern nodePattern = Pattern.compile("^\\s*([a-zA-Z0-9]*)\\s*\\[([a-zA-Z0-9=]*)]\\s*;");
+    private static final Pattern edgePattern = Pattern.compile("^\\s*([a-zA-Z0-9]*)\\s*->\\s*([a-zA-Z0-9]*)\\s*\\[([a-zA-Z0-9=]*)]\\s*;");
+
+    @Getter
+    @AllArgsConstructor
+    private static class Line {
+        private final LineType type;
+        private final Matcher m;
+    }
+
+    /**
+     * Takes a line and determines its type in addition to its Regex matcher object
+     * @param line Line to tokenize
+     *
+     * @return Line object
+     */
+    private static Line tokenize(String line) {
+        Matcher dMatcher = digraphPattern.matcher(line);
+        Matcher nMatcher = nodePattern.matcher(line);
+        Matcher eMatcher = edgePattern.matcher(line);
+
+        LineType type = LineType.OTHER;
+        Matcher matcher = null;
+
+        if (dMatcher.find()) {
+           type = LineType.DIGRAPH;
+           matcher = dMatcher;
+        } else if (nMatcher.find()) {
+           type = LineType.NODE;
+           matcher = nMatcher;
+        } else if (eMatcher.find()) {
+            type = LineType.EDGE;
+            matcher = eMatcher;
+        }
+
+        return new Line(type, matcher);
+    }
+
+    /**
+     * Convert an option string to a HashMap
+     * @param optionString Option string from GraphViz file
+     * @return HashMap representing options and their values
+     */
+    private static HashMap<String, String> optionsToHashmap(String optionString) {
+        HashMap<String, String> map = new HashMap<>();
+
+        for (String i: optionString.split(",")) {
+            String[] keyVal = i.split("=");
+            String key = keyVal[0].trim();
+            String value = keyVal[1].trim();
+
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
     public static Graph parse(String filename) throws IOException {
-        //presumably user inputs path to DOT file then number of threads to use
-        //start by getting the strings
-        List<String> nodeLines = new ArrayList<>();
-        List<String> edgeLines = new ArrayList<>();
-
+        // Create a buffered reader from the specified file
         File file = new File(filename);
-
         BufferedReader br = new BufferedReader(new FileReader(file));
 
-        // Name of the graph
-        String graphName = "digraph";
+        Graph graph = new Graph("digraph");
 
         String st;
         while ((st = br.readLine()) != null) {
-            if (st.contains("{")) {
-                // Pull name of graph from the String between double quotes
-                int start = st.indexOf("\"");
-                int end = st.lastIndexOf("\"");
-                String subString = st.substring(start+1, end);
+            Line l = tokenize(st);
 
-                // Ensure the graph name is non-empty before overriding the default
-                if (subString.trim().length() > 0) {
-                    graphName = subString;
-                }
-            } else if (st.contains("}")) {
-                break;
-            } else {
-                if (st.toLowerCase().contains("Weight".toLowerCase())) {
-                    if (st.contains("->")) {
-                        edgeLines.add(st);
-                    } else {
-                        nodeLines.add(st);
+            // Name of the node/digraph
+            String name;
+            // Option string as a hashmap
+            HashMap<String, String> options;
+
+            switch (l.getType()) {
+                case DIGRAPH:
+                    name = l.getM().group(1);
+
+                    if (name.trim().length() > 0) {
+                        graph.setName(name);
                     }
-                }
+
+                    break;
+                case NODE:
+                    name = l.getM().group(1);
+                    options = optionsToHashmap(l.getM().group(2));
+
+                    int value = Integer.parseInt(options.get("Weight"));
+                    graph.addNode(name, value);
+                    break;
+                case EDGE:
+                    String from = l.getM().group(1);
+                    String to = l.getM().group(2);
+
+                    options = optionsToHashmap(l.getM().group(3));
+
+                    int weight = Integer.parseInt(options.get("Weight"));
+                    graph.addEdge(weight, from, to);
             }
-        }
-
-        Graph graph = new Graph(graphName);
-
-        //Make nodes
-        for (String line : nodeLines) {
-            String myLine = line.replaceAll(" ", "");
-            myLine = myLine.replaceAll("\t", "");
-            String name = myLine.substring(0,myLine.indexOf('['));
-            String value = myLine.substring(myLine.indexOf('=') + 1,myLine.indexOf(']'));
-            graph.addNode(name, Integer.valueOf(value));
-        }
-
-        //Make edges
-        for (String line : edgeLines) {
-            String myLine = line.replaceAll(" ", "");
-            myLine = myLine.replaceAll("\t", "");
-            String start = myLine.substring(0,myLine.indexOf('-'));
-            String end = myLine.substring(myLine.indexOf('>') + 1,myLine.indexOf('['));
-            String value = myLine.substring(myLine.indexOf('=') + 1,myLine.indexOf(']'));
-            graph.addEdge(Integer.valueOf(value), start, end);
         }
 
         return graph;
