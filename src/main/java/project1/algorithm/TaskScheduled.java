0f922f5d87c663c734da1faec589a5b5b820c3f8
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-10 12:45:48 +1200

    refactor: Turn TaskScheduled into a dataclass
    All fields are final and we use a Lombok annotation to create a constructor.

--- src/main/java/project1/algorithm/TaskScheduled.java
+++ src/main/java/project1/algorithm/TaskScheduled.java
@@ -1,36 +1,29 @@
 package project1.algorithm;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import project1.graph.Node;
 
 /**
  * This class represents a node/task after it has been scheduled to
  * a processor.
+ *
+ * task      Task node that appears in a schedule/sub-schedule.
+ * start     The time when the task node will start its job
+ * processor The processor which the task node is assigned to.
  */
 @Getter
+@AllArgsConstructor
 public class TaskScheduled {
-    Node taskNode;
-    int startingTime;
-    int processor;
-
-    /**
-     * A constructor method that maintains information about a scheduled task for a schedule.
-     * @param task task node that appears in a schedule/sub-schedule.
-     * @param start the time when the task node will start its job
-     * @param processor the processor which the task node is assigned to.
-     */
-    public TaskScheduled(Node task,int start,int processor){
-        this.taskNode=task;
-        this.startingTime=start;
-        this.processor=processor;
-    }
+    final Node taskNode;
+    final int startingTime;
+    final int processor;
 
     /**
      * This method computes and returns the finishing time of this scheduled task object.
      * @return The finishing time of this already scheduled task node.
      */
     public int getFinishTime(){
-        return this.startingTime+ taskNode.getWeight();
+        return this.startingTime + taskNode.getWeight();
     }
-
 }
