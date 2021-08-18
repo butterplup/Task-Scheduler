7275cfdf42ca6cc8295f8bdacf0a6321f97c62e7
Joel Hutchinson <joelkahuhutch@gmail.com>
2021-08-19 03:59:55 +1200

    feat: method to display best schedule written, not linked to the rest of the application yet

---
+++ src/main/java/project1/Visualiser.java
@@ -0,0 +1,19 @@
+package project1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Visualiser extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Team 2: Electric Boogaloo");
+
+
+    }
+}
