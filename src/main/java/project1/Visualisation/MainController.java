e585257daef468b7c3fe222641cbc74ad3eb85bb
Kieran-Byte <bettesworthkieran1@gmail.com>
2021-08-19 17:05:19 +1200

    Created a controller for the fxml file

---
+++ src/main/java/project1/Visualisation/MainController.java
@@ -0,0 +1,38 @@
+package project1.Visualisation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class MainController {
+
+    @FXML
+    private Text PlaceHolderText;
+
+    @FXML
+    private Text TimeText;
+
+    @FXML
+    private Text StatusText;
+
+    @FXML
+    private VBox ganttBox;
+
+    @FXML
+    private HBox allocBox;
+
+    @FXML
+    private HBox orderBox;
+
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private TextField outputField;
+
+    @FXML
+    private TextField numProField;
+
+}
