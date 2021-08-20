540b03b1985cfb3a199735c7a046a4a2d832ad63
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-20 20:03:48 +1200

    fix: Remove typecasting in TopologicalSort

--- src/main/java/project1/algorithm/TopologicalSort.java
+++ src/main/java/project1/algorithm/TopologicalSort.java
@@ -11,40 +11,30 @@ import java.util.List;
 
 public class TopologicalSort {
 
-    public TopologicalSort(){
-    }
-
     //Using Kahn's algorithm
-    public PartialSchedule getSchedule(Graph g, int processorsCount){
-
-        LinkedList<Node> nodesDone = new LinkedList<>();
-
-        ArrayList<Node> tempNodes = (ArrayList<Node>) g.getNodes();
+    public static PartialSchedule getSchedule(Graph g, int processorsCount){
+        // Shallow copy of graph nodes
+        LinkedList<Node> nodesDone = new LinkedList<>(g.getNodes());
 
         //Sorts list of nodes from graph based on critical path
-        Collections.sort(tempNodes, (n1, n2) -> n2.getCriticalPath() - n1.getCriticalPath());
+        nodesDone.sort((n1, n2) -> n2.getCriticalPath() - n1.getCriticalPath());
 
-        //Converts array list to linked list
-        for(Node n: tempNodes){
-            nodesDone.add(n);
-        }
-
-        PartialSchedule topoSchedule=new PartialSchedule(g,processorsCount);
+        PartialSchedule topoSchedule = new PartialSchedule(g, processorsCount);
 
         while (!nodesDone.isEmpty()){
-            Node currentNode= nodesDone.removeFirst();  //Get a node to schedule
+            Node currentNode= nodesDone.removeFirst();  // Get a node to schedule
 
             int index=-1;
             int bestProcessor=index;
             int currentEarliest=Integer.MAX_VALUE;
             TaskScheduled[] scheduled= topoSchedule.getScheduledTasks();
-            int[] procinfo=topoSchedule.getProcessorInfo();
 
-            //go through all processors,Use earliestStartTime as selection rule
+            // Go through all processors,Use earliestStartTime as selection rule
             for (int free : topoSchedule.getProcessorInfo()) {
                 index++; //processor number
                 int startTime;
                 int communicationCost = 0;
+
                 // Find the earliest starting time on this processor p
                 for (Edge e : currentNode.getIncomingEdges()) {
                     Node pre = e.getStart(); // Get parent node
@@ -53,22 +43,24 @@ public class TopologicalSort {
                         communicationCost = Math.max(communicationCost, predecessor.getFinishTime() + e.getWeight());
                     }
                 }
-                startTime = Math.max(free, communicationCost);
 
-                if (startTime<currentEarliest){
-                    currentEarliest=startTime;
-                    bestProcessor=index;
+                startTime = Math.max(free, communicationCost);
+                if (startTime < currentEarliest){
+                    currentEarliest = startTime;
+                    bestProcessor = index;
                 }
 
-                if (currentEarliest==0){
+                if (currentEarliest == 0){
                     //can not have better EST time
                     break;
                 }
             }
+
             //Assign task to this processor
             TaskScheduled ts = new TaskScheduled(currentNode, currentEarliest, bestProcessor);
-            topoSchedule=new PartialSchedule(topoSchedule,ts);
+            topoSchedule = new PartialSchedule(topoSchedule,ts);
         }
+
         return topoSchedule;
     }
 }
