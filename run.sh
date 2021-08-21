ed7aea2747c49e647dfbb0122b0e2bb01a140046
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-21 14:12:29 +1200

    fix: Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 mvn package
-java -jar target/project1-1.0-Milestone1.jar src/main/java/test.dot
+java -jar target/project1-1.0-Milestone1.jar src/test/resources/exampleTaskGraphs/Nodes_11_OutTree.dot 4 -p 4
