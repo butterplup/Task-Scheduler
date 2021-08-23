750c6f08ac01d697dc45b3fec3cf23cc6c11dbac
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-08-23 21:06:11 +1200

    fix: Remove redundant operating system bean from MemTile

--- src/main/java/project1/visualisation/tiles/MemTile.java
+++ src/main/java/project1/visualisation/tiles/MemTile.java
@@ -2,19 +2,14 @@ package project1.visualisation.tiles;
 
 import javafx.scene.layout.Pane;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
-
 public class MemTile extends BarGaugeTile {
-    private final OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
-
     public MemTile(Pane parent) {
         super(parent, "Current Memory Usage",  -1);
     }
 
     @Override
     public void update() {
-        ///updates the memory in the memory tile
+        /// updates the memory in the memory tile
         double memoryUsage = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/(1000000d);
         getTile().setValue(memoryUsage);
     }
